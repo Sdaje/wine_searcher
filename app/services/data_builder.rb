7c7e175784145606f1c224c0a02591e11c3ca737
Sylvain Dajeans <sylvain.dajeans@gmail.com>
2023-02-10 19:20:32 +0100

    feat(data builder-service): add service to build instances from scrapping datas

---
+++ app/services/data_builder.rb
@@ -0,0 +1,29 @@
+class DataBuilder
+  def initialize(source_data)
+    @source_data = source_data
+  end
+
+  def run
+    @source_data.each { |element| find_or_generate_instances_from!(element) }
+  end
+
+  private
+
+  def find_or_generate_instances_from!(element)
+    bottle = Bottle.find_by(name: element[:bottle][:name]).presence || Bottle.new(element[:bottle])
+    price = Price.new(element[:price])
+    price.bottle = bottle
+    return if bottle.invalid? || price.invalid?
+
+    element[:tags].each { |category, value| find_or_generate_tag_from!(category, value, bottle) }
+    price.save # save bottle if not persisted [Sylvain Dajeans]
+  end
+
+  def find_or_generate_tag_from!(category, value, bottle)
+    tag = Tag.find_by(value:).presence || Tag.new(category:, value:)
+    bottle_tag = BottleTag.find_by(bottle:, tag:).presence || BottleTag.new(bottle:, tag:)
+    return if bottle_tag.persisted?
+
+    bottle_tag.save if tag.valid? # save tag if not persisted [Sylvain Dajeans]
+  end
+end
